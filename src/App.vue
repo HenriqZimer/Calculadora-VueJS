ba9074e13bc7b68da90c85eef72fc5649a395fa6
Henrique Zimermann <henriquezimermanngeo@gmail.com>
2023-07-18 15:50:45 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>Calculadora</h1>
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from "./main/Calculator.vue";
+export default {
+    components: {
+        Calculator
+    }
+}
+</script>
+<style>
+body{
+    margin: 0;
+} 
+#app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: #fff;
+    background-color: rgb(0, 0, 255) !important;
+}
+</style> 
